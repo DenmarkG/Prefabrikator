497963095f10480ee82cb74cbc52f0023237ea4c
Superdenny <denmark.s.gibbs@gmail.com>
2022-04-27 22:53:53 -0700

    added shared value

---
+++ Assets/Code/Util/SharedValue.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Shared<T>
+{
+    public T Value => _value;
+    private T _value = default(T);
+
+    public event Action<T> OnValueChanged = null;
+
+    public Shared(T t = default(T))
+    {
+        _value = t;
+    }
+
+    public Shared(T t, Action<T> onValueChaned)
+    {
+        _value = t;
+        OnValueChanged = onValueChaned;
+    }
+
+    public static implicit operator T(Shared<T> t) => t._value;
+    public void Set(T t)
+    {
+        _value = t;
+        OnValueChanged?.Invoke(_value);
+    }
+}
