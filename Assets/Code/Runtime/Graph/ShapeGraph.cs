0b3c7c7093978e92d05cc6a182887d44970cbe5e
Superdenny <denmark.s.gibbs@gmail.com>
2025-06-26 08:20:37 -0700

    [UPDATE] fix for comiple error

--- Assets/Code/Runtime/Graph/ShapeGraph.cs
+++ Assets/Code/Runtime/Graph/ShapeGraph.cs
@@ -4,27 +4,27 @@ using System.Collections.Generic;
 namespace Prefabrikator
 {
 
-    public class Node
-    {
-        public List<Node> Inputs { get; private set; } = new();
-        public HashSet<Type> Outputs { get; private set; }
+    //public class Node
+    //{
+    //    public List<Node> Inputs { get; private set; } = new();
+    //    public HashSet<Type> Outputs { get; private set; }
 
-        public void AddInput(Node input)
-        {
-            if (input != null)
-            {
-                Inputs.Add(input);
-            }
-        }
+    //    public void AddInput(Node input)
+    //    {
+    //        if (input != null)
+    //        {
+    //            Inputs.Add(input);
+    //        }
+    //    }
 
-        public void Execute()
-        {
-            //
-        }
-    }
+    //    public void Execute()
+    //    {
+    //        //
+    //    }
+    //}
 
-    public class ShapeGraph
-    {
-        //
-    }
+    //public class ShapeGraph
+    //{
+    //    //
+    //}
 }
